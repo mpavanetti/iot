1436bdaed99c305bd8d73903176a0bb5d5c10254
Matheus Pavanetti <matheus.pavanetti@gmail.com>
2023-08-27 18:47:15 -0600

    improving basic program for reading data from bme280.

--- iot_source/picow/main.py
+++ iot_source/picow/main.py
@@ -1,14 +1,26 @@
-# install module micropython_bme280
 from machine import Pin, I2C        #importing relevant modules & classes
 from time import sleep
 import bme280       #importing BME280 library
 
 i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)    #initializing the I2C method 
 led = Pin(2, Pin.OUT)
+pico_led = Pin("LED", Pin.OUT)
 
-while True:
-  led.value(1)  
-  bme = bme280.BME280(i2c=i2c)
-  print(bme.values)
-  led.value(0)
-  sleep(5)           
+try:
+    while True:
+      pico_led.value(1)
+      led.value(1)  
+      bme = bme280.BME280(i2c=i2c)
+      print(bme.values)
+      led.value(0)
+      sleep(2)
+except Exception as e:
+    print("[*] Program Aborted.")
+    print(f"Exception(Exception): {e}")
+    pico_led.value(0)
+except KeyboardInterrupt:
+    print("[*] Keyboard Program Interrupted.")
+    print(f"Exception(KeyboardInterrupt)")
+    pico_led.value(0)
+    
+    
