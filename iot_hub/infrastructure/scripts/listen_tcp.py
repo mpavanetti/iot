53dd149de7dbabc545f55875a5eb55d28d069088
Matheus Pavanetti <matheuspavanetti@gmail.com>
2023-09-08 17:14:47 +0000

    adjusting kafka listeners and listen_tcp script.

--- iot_hub/infrastructure/scripts/listen_tcp.py
+++ iot_hub/infrastructure/scripts/listen_tcp.py
@@ -3,18 +3,20 @@ from kafka import KafkaProducer
 from os import getenv
 from datetime import datetime
 from logging import info, warning, error
+from time import sleep
 
 # Socket Port
 port = int(getenv("SOCKET_PORT", 1500))
 
 # Define server with port
-#bootstrap_servers = getenv("BOOTSTRAP_SERVERS", "kafka:9092")
+#bootstrap_servers = getenv("BOOTSTRAP_SERVERS", "kafka:9092") # internal
 bootstrap_servers = getenv("BOOTSTRAP_SERVERS", "localhost:29092") # external
 
 # Define topic name where the message will publish
 topicName = getenv("KAFKA_TOPIC", "iot_source")
 
-now = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+# get sleep time before start poking
+wait_time = int(getenv("WAIT_TIME", 50))
 
 # Create Socket
 sock = socket()
@@ -26,21 +28,25 @@ sock.bind(("", port))
 sock.listen(5)
 warning(f"[*] Socket is listening at port {port}.")
 
+# wait kafka kraf startup before start the kafka producer
+sleep(wait_time)
+
 # Initialize producer variable
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers,security_protocol="PLAINTEXT")
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                         security_protocol="PLAINTEXT")
 
 # Infinite Loop
 while True:
     c, addr = sock.accept()
-    warning(f"[CONNECTION] Received from: {addr} at {now}")
+    warning(f"[CONNECTION] Received from: {addr} at {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}")
 
     payload = c.recv(1024)
-    print(f"[PAYLOAD] Received:  ", payload)
+    info(f"[PAYLOAD] Received:  ", payload)
 
     try:
     # Kafka Produce message
         producer.send(topicName, payload)
-        info(f"Message Sent at {now}.")
+        info(f"Message Sent at {datetime.now().strftime('%Y/%m/%d %H:%M:%S')}.")
         producer.flush()
         
     except BaseException as e:
