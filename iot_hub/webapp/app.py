45366c8048ee6c80ee676aa781841d65cee7d74f
Matheus Pavanetti <matheuspavanetti@gmail.com>
2023-09-20 15:35:03 -0600

    adding streaming chart.

--- iot_hub/webapp/app.py
+++ iot_hub/webapp/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, redirect
-import platform
-import socket
-
+from flask import Flask, render_template, redirect, Response, request, stream_with_context
 from application.Hardware import Hardware
+
+import logging
+import random
+import sys
+import time
+import json
+from datetime import datetime
+from typing import Iterator
+from kafka import KafkaConsumer
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
 
 
 app = Flask(__name__)
+
+consumer = KafkaConsumer(
+    'iot_source',
+    bootstrap_servers=['192.168.1.80:29092'],
+    auto_offset_reset = 'latest',
+    group_id=None,
+)
 
 @app.route('/')
 def routing():
@@ -23,6 +39,46 @@ def hardware():
                            network=hardware.get_all_network(),
                            hardware= hardware.get_all_hardware(),
                           python=hardware.get_all_python())
+
+@app.route('/streaming')
+def streaming():
+     return render_template('streaming.html',
+                            menu="streaming",)
+
+
+
+def generate_random_data() -> Iterator[str]:
+    """
+    Generates random value between 0 and 100
+
+    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
+    """
+    if request.headers.getlist("X-Forwarded-For"):
+        client_ip = request.headers.getlist("X-Forwarded-For")[0]
+    else:
+        client_ip = request.remote_addr or ""
+
+    try:
+        logger.info("Client %s connected", client_ip)
+        #while True:
+        for msg in consumer:
+            data = json.loads(msg.value.decode("utf-8"))
+            json_data = json.dumps({"time": data["bme280"]["read_datetime"],
+                                    "value": float(data["bme280"]["temperature"].replace("C",""))})
+
+            
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+    except GeneratorExit:
+        logger.info("Client %s disconnected", client_ip)
+
+
+@app.route("/chart-data")
+def chart_data() -> Response:
+    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8001, host='0.0.0.0') 
