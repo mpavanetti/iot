c8eccc9d419d7032272536603a72ef4a80c2a92f
Matheus Pavanetti <matheus.pavanetti@gmail.com>
2023-09-22 11:08:48 -0600

    created a separated class for streaming data.

--- iot_hub/webapp/app.py
+++ iot_hub/webapp/app.py
@@ -1,99 +1,74 @@
-from flask import Flask, render_template, redirect, Response, request, stream_with_context
+from flask import Flask, render_template, redirect, Response, stream_with_context
 from application.Hardware import Hardware
+from application.Streaming import Streaming
 
-import logging
-import sys
-import time
-import json
-from datetime import datetime
-from typing import Iterator
-from kafka import KafkaConsumer
-
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
-logger = logging.getLogger(__name__)
-
-consumer = KafkaConsumer(
-            'iot_source',
-            bootstrap_servers=['kafka:9092'],
-            auto_offset_reset = 'latest',
-            group_id=None,
-)
-
+# Generic Variables
+kafka_broker_list = ["localhost:29092"]
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def routing():
-    return redirect('/home')
+    return redirect("/home")
 
-@app.route('/home')
+
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/api/check_host_status')
+
+@app.route("/api/check_host_status")
 def host_status():
     hardware = Hardware()
-    dataset = {"local_ip": hardware.get_local_ip(),
-               "picow_ip": hardware.picow_ip,
-                "local_alive": hardware.check_if_host_alive(hardware.get_local_ip()),
-                "picow_alive": hardware.check_if_host_alive(hardware.picow_ip)}
+    dataset = {
+        "local_ip": hardware.get_local_ip(),
+        "picow_ip": hardware.picow_ip,
+        "local_alive": hardware.check_if_host_alive(hardware.get_local_ip()),
+        "picow_alive": hardware.check_if_host_alive(hardware.picow_ip),
+    }
     return dataset
 
-@app.route('/api/load_host_hardware')
+
+@app.route("/api/load_host_hardware")
 def host_hardware():
     hardware = Hardware()
-    dataset = {"cpu_usage": hardware.get_cpu_usage(),
-                "mem_usage": hardware.get_mem_usage(),
-                "disk_usage": hardware.get_disk_usage()}
+    dataset = {
+        "cpu_usage": hardware.get_cpu_usage(),
+        "mem_usage": hardware.get_mem_usage(),
+        "disk_usage": hardware.get_disk_usage(),
+    }
     return dataset
- 
 
-@app.route('/hardware')
+
+@app.route("/hardware")
 def hardware():
-    hardware= Hardware()
-    return render_template('hardware.html',
-                           network=hardware.get_all_network(),
-                           hardware= hardware.get_all_hardware(),
-                           python=hardware.get_all_python())
+    hardware = Hardware()
+    return render_template(
+        "hardware.html",
+        network=hardware.get_all_network(),
+        hardware=hardware.get_all_hardware(),
+        python=hardware.get_all_python(),
+    )
 
-@app.route('/streaming')
+
+@app.route("/streaming")
 def streaming():
-     return render_template('streaming.html')
-
-
-
-def generate_random_data() -> Iterator[str]:
-    """
-    Generates random value between 0 and 100
-
-    :return: String containing current timestamp (YYYY-mm-dd HH:MM:SS) and randomly generated data.
-    """
-    if request.headers.getlist("X-Forwarded-For"):
-        client_ip = request.headers.getlist("X-Forwarded-For")[0]
-    else:
-        client_ip = request.remote_addr or ""
-
-    try:
-        logger.info("Client %s connected", client_ip)
-        #while True:
-        for msg in consumer:
-            data = json.loads(msg.value.decode("utf-8"))
-            json_data = json.dumps({"time": data["bme280"]["read_datetime"],
-                                    "value": float(data["bme280"]["temperature"].replace("C",""))})
-
-            
-            yield f"data:{json_data}\n\n"
-            time.sleep(1)
-    except GeneratorExit:
-        logger.info("Client %s disconnected", client_ip)
+    return render_template("streaming.html")
 
 
 @app.route("/chart-data")
 def chart_data() -> Response:
-    response = Response(stream_with_context(generate_random_data()), mimetype="text/event-stream")
+    streaming = Streaming()
+    streaming.kafka_connect("iot_source", kafka_broker_list)
+    response = Response(
+        stream_with_context(streaming.iterate_kafka_data()),
+        mimetype="text/event-stream",
+    )
     response.headers["Cache-Control"] = "no-cache"
     response.headers["X-Accel-Buffering"] = "no"
     return response
 
-if __name__ == '__main__':
-	app.run(debug=True, port=8001, host='0.0.0.0') 
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8001, host="0.0.0.0")
---
+++ iot_hub/webapp/application/Streaming.py
@@ -0,0 +1,48 @@
+from kafka import KafkaConsumer
+from typing import Iterator
+from flask import request
+import logging
+import json
+import time
+import sys
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+class Streaming:
+    def __init__(self):
+        pass
+
+    def kafka_connect(self, topic: str, servers: list):
+        consumer = KafkaConsumer(
+            topic, bootstrap_servers=servers, auto_offset_reset="latest", group_id=None
+        )
+        self.consumer = consumer
+
+    def iterate_kafka_data(self) -> Iterator[str]:
+        if request.headers.getlist("X-Forwarded-For"):
+            client_ip = request.headers.getlist("X-Forwarded-For")[0]
+        else:
+            client_ip = request.remote_addr or ""
+
+        try:
+            logger.info("Client %s connected", client_ip)
+            # while True:
+            for msg in self.consumer:
+                data = json.loads(msg.value.decode("utf-8"))
+                json_data = json.dumps(
+                    {
+                        "time": data["bme280"]["read_datetime"],
+                        "value": float(data["bme280"]["temperature"].replace("C", "")),
+                    }
+                )
+
+                yield f"data:{json_data}\n\n"
+                time.sleep(1)
+        except GeneratorExit:
+            logger.info("Client %s disconnected", client_ip)
