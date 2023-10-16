23d8ed379fc8846d6fe700a713e628e3a1c5cbba
Matheus Pavanetti <matheuspavanetti@gmail.com>
2023-10-16 14:04:04 -0600

    adding sh initialization.

--- iot_hub/infrastructure/cron/scripts/run_process_iot_spark.sh
+++ iot_hub/infrastructure/cron/scripts/run_process_iot_spark.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 docker compose -f /home/docker-compose.yml -p iot exec spark spark-submit \
 --jars /home/jovyan/work/jars/commons-pool2-2.11.1.jar,/home/jovyan/work/jars/spark-sql-kafka-0-10_2.12-3.4.1.jar,/home/jovyan/work/jars/kafka-clients-3.5.1.jar,/home/jovyan/work/jars/spark-token-provider-kafka-0-10_2.12-3.4.1.jar,/home/jovyan/work/jars/mysql-connector-j-8.0.31.jar \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,org.apache.kafka:kafka-clients:3.2.1 \
